47b2080dc60341932b087d407ec47817a2550364
Tarun <sirtarunkumar7@gmail.com>
2026-09-05 23:11:27 +0530

    doing A_bit question

---
+++ A_Bit.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int ans=0;
+    while(n--)
+    {
+        string s;
+        cin>>s;
+
+        if(s=="++X" || s=="X++")
+        {
+            ans++;
+        }
+        else if(s=="--X" || s=="X--")
+        {
+            ans--;
+        }
+    }
+    cout<<ans<<endl;
+}
